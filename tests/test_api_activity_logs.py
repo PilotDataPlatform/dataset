05497fa2d732a4ac6e224c88a3471ccc68a62c9b
Hugo Antunes <you@example.com>
2022-04-27 15:58:21 +0200

    fix linter

--- tests/test_api_activity_logs.py
+++ tests/test_api_activity_logs.py
@@ -13,26 +13,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
 import json
+
+import pytest
+
 pytestmark = pytest.mark.asyncio
 
 
 async def test_activity_logs_should_get_data_from_elasticsearch(client, httpx_mock):
-    query = json.dumps({
-        "dataset_geid": {"value": "679e4b11-61fd-4df7-9084-5e0ff0c99b9e-1647523203", "condition": "equal"}
-    })
+    query = json.dumps(
+        {'dataset_geid': {'value': '679e4b11-61fd-4df7-9084-5e0ff0c99b9e-1647523203', 'condition': 'equal'}}
+    )
     query_string = f'page_size=10&page=0&order_by=create_timestamp&order_type=desc&query={query}'
 
     httpx_mock.add_response(
         method='GET',
         url='http://elastic_search_service/activity-logs/_search',
-        json={
-            'hits': {
-                'hits': {'any': 'any'},
-                'total': {'value': 1}
-            }
-        }
+        json={'hits': {'hits': {'any': 'any'}, 'total': {'value': 1}}},
     )
     res = await client.get(f'/v1/activity-logs?{query_string}')
     assert res.status_code == 200
@@ -55,12 +52,7 @@ async def test_activity_logs_get_by_version_should_return_200(client, httpx_mock
     httpx_mock.add_response(
         method='GET',
         url='http://elastic_search_service/activity-logs/_search',
-        json={
-            'hits': {
-                'hits': {'any': 'any'},
-                'total': {'value': 1}
-            }
-        }
+        json={'hits': {'hits': {'any': 'any'}, 'total': {'value': 1}}},
     )
 
     res = await client.get(f'/v1/activity-logs/{dataset_geid}?{query_string}')
