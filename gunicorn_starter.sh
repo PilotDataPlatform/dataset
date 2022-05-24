69be40b6e09e391612e0f351373f4801035de446
Dušan Andrić <dandric@indocresearch.org>
2022-05-24 14:15:58 -0400

    Update gunicorn_starter.sh

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (C) 2022 Indoc Research
 #
 # This program is free software: you can redistribute it and/or modify
@@ -12,8 +13,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#!/bin/bash
 
 set -e
 
