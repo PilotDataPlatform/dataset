45b9058bc39088b8efffe70e03167db3a6cb6302
agu <agu@indocresearch.org>
2022-07-11 12:51:27 -0400

    git fix pre-commit

--- migrations/versions/ca6c52333938_update_bids_result_table.py
+++ migrations/versions/ca6c52333938_update_bids_result_table.py
@@ -1,14 +1,10 @@
-"""update bids_result table
+"""update bids_result table.
 
 Revision ID: ca6c52333938
 Revises: ad96d8573c01
 Create Date: 2022-07-11 11:59:20.714018
-
 """
 from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-
 
 # revision identifiers, used by Alembic.
 revision = 'ca6c52333938'
@@ -19,7 +15,11 @@ depends_on = None
 
 def upgrade():
     op.create_unique_constraint(
-        "dataset_geid", "bids_results", ["dataset_geid"], schema='dataset',)
+        'dataset_geid',
+        'bids_results',
+        ['dataset_geid'],
+        schema='dataset',
+    )
 
 
 def downgrade():
